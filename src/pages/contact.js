336e98630a8a97c9279dc8b632badbea79a09c13
Ram Dettmer <ramdettmer751@gmail.com>
2021-01-02 23:07:31 -0800

    init

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default function Contact () {
+    return (
+        <div>
+            <Link to="/">Back Home</Link>
+            <Header headerText="Contact Page" />
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact">Contact</Link>
+      <Header headerText="Heyyy" />
+      <p>What a world!</p>
+      <img src="https://source.unsplash.com/random/400x200"/>
+    </div>
+  )
 }
